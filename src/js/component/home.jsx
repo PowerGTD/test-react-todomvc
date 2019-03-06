5be29fd61c1331ae0b408b6506c81d0bd9313619
Greg DePersio <greg.depersio@gmail.com>
2019-03-06 20:37:49 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,30 +5,95 @@ export class Home extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			contacts: ["Chase girls", "Code cool stuff", "Crush weights"],
-			input: ""
+			contacts: [],
+			input: "",
+			userName: ""
 		};
 	}
 
 	addListItems = e => {
 		let key = e.which || e.keyCode || 0;
-		if (key === 13) {
-			this.setState({ input: e.target.value });
+		if (key !== 13) {
+			return;
+		} else {
 			let newContacts = this.state.contacts;
-			newContacts.push(e.target.value);
-			this.setState({
-				contacts: newContacts
-			});
-			e.target.value = "";
+			let newObject = { label: this.state.input, done: false };
+			newContacts.push(newObject);
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/" +
+					this.state.userName,
+				{
+					method: "PUT",
+					body: JSON.stringify(newContacts),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				}
+			)
+				.then(newRes => newRes.text())
+				.then(response => {
+					console.log(response);
+					this.setState({
+						contacts: newContacts,
+						input: ""
+					});
+				})
+				.catch(error => console.error("Error:", error));
 		}
 	};
 
 	deleteListItems = index => {
 		let updatedContacts = this.state.contacts;
-		updatedContacts.splice(index, 1);
-		this.setState({
-			contacts: updatedContacts
-		});
+		updatedContacts[index].done = true;
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/" +
+				this.state.userName,
+			{
+				method: "PUT",
+				body: JSON.stringify(updatedContacts),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			.then(newRes => newRes.text())
+			.then(response => {
+				console.log(response);
+				this.setState({
+					contacts: updatedContacts
+				});
+			})
+			.catch(error => console.error("Error:", error));
+	};
+
+	updateUserName = e => {
+		let key = e.which || e.keyCode || 0;
+		if (key !== 13) {
+			return;
+		} else {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/" +
+					this.state.userName
+			)
+				.then(newRes => newRes.json())
+				.then(nob => {
+					if (Array.isArray(nob) === true) {
+						this.setState({ contacts: nob });
+					} else {
+						fetch(
+							"https://assets.breatheco.de/apis/fake/todos/user/" +
+								this.state.userName,
+							{
+								method: "POST",
+								body: JSON.stringify([]),
+								headers: {
+									"Content-Type": "application/json"
+								}
+							}
+						).then(() => this.setState({ contacts: [] }));
+					}
+				});
+		}
 	};
 
 	render() {
@@ -37,18 +102,39 @@ export class Home extends React.Component {
 				<div className="header">
 					<h1>todos</h1>
 				</div>
+				<div className="userNameChanger">
+					<div className="userNameLabel">
+						<h3>Username:</h3>
+					</div>
+					<div className="userNameInput">
+						<input
+							id="addUser"
+							type="text"
+							placeholder="What is your username?"
+							onKeyPress={this.updateUserName}
+							value={this.state.userName}
+							onChange={e =>
+								this.setState({ userName: e.target.value })
+							}
+						/>
+					</div>
+				</div>
 				<div className="container">
 					<input
 						id="addItem"
 						type="text"
 						placeholder="What needs to be done?"
 						onKeyPress={this.addListItems}
+						value={this.state.input}
+						onChange={e => this.setState({ input: e.target.value })}
 					/>
 					<ul>
 						{this.state.contacts.map((item, index) => {
 							return (
 								<li className="flexItems" key={index}>
-									<div className="listItem">{item}</div>
+									<div className="listItem">
+										{item.label} {item.done && " - Done"}
+									</div>
 									<div className="deleter">
 										<span
 											onClick={() =>
